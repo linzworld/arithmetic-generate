cbc76fa3d6e434b1a1ac9644a60c6686a8a409c0
linzworld <2448974318@qq.com>
2020-04-03 09:10:25 +0800

    initialize:初始化项目

---
+++ arithmetic-generate/src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Description 测试
+ * @Author 林泽鸿
+ * @Date 2020/4/3 9:06
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试。。");
+    }
+}
